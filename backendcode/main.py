ce74c08cdca299ac877e4beff54d27425294fcf5
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2025-01-31 09:50:43 -0500

    doesn't work

--- backendcode/main.py
+++ backendcode/main.py
@@ -1,4 +1,3 @@
-# Import flask and flask_cors
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
@@ -10,25 +9,22 @@ import answerchecker as ac
 import json
 import requests
 
-
 app = Flask(__name__)  # Create Flask app
-CORS(app)  # Apply CORS to the entire app
+CORS(app, resources={r"/tossup": {"origins": "http://127.0.0.1:5500"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/tossup', methods=['GET'])  # Random tossup
-@cross_origin()
+@cross_origin(origin='127.0.0.1',headers=['Content-Type','Authorization'])
 def tossup():
     tossup = gc.get_tossup()  # Get tossup
     return json.dumps(tossup)
 
 @app.route('/bonus', methods=['GET'])  # Random bonus
-@cross_origin()
 def bonus():
     bonus = gc.get_bonuses()  # Get bonuses
     return json.dumps(bonus)
 
 @app.route('/checkanswer', methods=['GET'])  # Check answer
-@cross_origin()
 def checkanswer():
     question_id = request.args.get('questionid')  # get question id from query 
     question_obj = requests.get("https://qbreader.org/api/tossup-by-id", params={'id': question_id}).json()['tossup']  # get question object from API
@@ -37,3 +33,6 @@ def checkanswer():
     user_input = request.args.get('guess')  # get user input from query
 
     return json.dumps(ac.check_answer(user_input, correct_ans))
+
+if __name__ == '__main__':
+    app.run(debug=True)
