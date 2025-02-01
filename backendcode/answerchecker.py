8460a6f39ba327c8acaa997d1ba580ead631aa21
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2025-02-01 17:22:57 -0500

    works - but not

--- backendcode/answerchecker.py
+++ backendcode/answerchecker.py
@@ -7,7 +7,6 @@ def check_answer(user_input, correct_ans):
         "answerline": correct_ans
     }
     api_return = requests.get(url, params=data).json()
-    print(api_return)
     directive = api_return["directive"]
     directedPrompt = api_return["directedPrompt"] if directive == "prompt" else None
     return {"directive": directive, "directedPrompt": directedPrompt}
--- questionspage/scripts.js
+++ questionspage/scripts.js
@@ -67,20 +67,23 @@ $(document).ready(() => {
         var startedReading = false;
         var reading = false;
         var buzzing = false;
+        var wordindex = 0;
+        var beforePower = true;
 
-        var print = (words, i) => {
-            var readWord = false;
-            while (!readWord) {
-                if (reading) {
-                    var questionElm = $('#question');
-                    questionElm.append(words[i] + " ");
-                    readWord = true;
-    
-                    if (i < words.length - 1) {
-                        setTimeout(() => print(words, i + 1), 150);
-                    }
+
+        var print = (words) => {
+            if (reading) {
+                var questionElm = $('#question');
+                if (words[wordindex]) {
+                    if (words[wordindex] === "(*)") { beforePower = false; }
+                    else { questionElm.append(words[wordindex] + " "); }
+                }
+                wordindex += 1;
+
+                if (wordindex < words.length) {
+                    setTimeout(() => print(words), 200);
                 } else {
-                    continue;
+                    reading = false;
                 }
             }
         }
@@ -88,14 +91,14 @@ $(document).ready(() => {
         var readQuestion = () => {
             startedReading = true;
             reading = true;
-            print(questionArray, 0);
+            print(questionArray);
         }
 
         var userBuzz = () => {
             buzzing = true;
             reading = false;
             var actionsElm = $('#actions');
-            actionsElm.append("BUZZ! ");
+            actionsElm.append("<p>buzzed</p><br>"+actionsElm.html());
 
             // Show the answer container
             $('#answer-container').show();
@@ -106,13 +109,37 @@ $(document).ready(() => {
                         var directive = data["directive"];
                         var directedPrompt = data["directedPrompt"];
                         console.log(directive, directedPrompt);
+
+                        if (directive === "accept") {
+                            // Hide answer container
+                            $('#answer-container').hide();
+                            
+                            // Tell the user they answered correctly
+                            var answered = `Answered: ${$('#answer-input').val()}`;
+                            actionsElm.append(`<p>${answered}</p><br>`+actionsElm.html());
+                            actionsElm.append("<p>Answered correctly for 10 points</p><br>"+actionsElm.html());
+                        } else if (directive === "prompt") {
+                            // Tell the user they need to prompt
+                            var answered = `Answered: ${$('#answer-input').val()}`;
+                            actionsElm.append(`<p>${answered}</p><br>`+actionsElm.html());
+                            if (directedPrompt) {
+                                var prompt = `Prompt: ${directedPrompt}`
+                                actionsElm.append(`<p>${prompt}</p><br>`+actionsElm.html());
+                            }
+                            actionsElm.append("<p>prompted</p><br>"+actionsElm.html());
+                        } else if (directive === "reject") {
+                            // Hide answer container
+                            $('#answer-container').hide();
+
+                            // Tell the user they answered incorrectly
+                            var answered = `Answered: ${$('#answer-input').val()}`;
+                            actionsElm.append(`<p>${answered}</p><br>`+actionsElm.html());
+                            actionsElm.append("<p>Answered incorrectly for no penalty</p><br>"+actionsElm.html());
+                        }
                     });
 
-                    // Hide the answer container
-                    $('#answer-container').hide();
-
                     // Resume reading the question
-
+                    readQuestion();
                 }
             })
         }
@@ -121,8 +148,13 @@ $(document).ready(() => {
             if (event.key === 'n') {
                 if (!startedReading) { readQuestion() }
             } else if (event.key === ' ') {
-                if (!buzzing) { userBuzz() }
+                if (!buzzing) { if(reading) { userBuzz() } }
+            } else if (event.key === 'p') {
+                if (!buzzing) {
+                    if(reading) { reading = false; }
+                    else if(!reading) { reading = true; }
+                }
             }
-        });
-    });
-})
+        }
+    )});
+});
