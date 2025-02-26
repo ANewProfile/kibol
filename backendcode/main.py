aeb6b8ca5c2ad87709b5bf35661d0b51520c5b93
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2025-02-26 06:10:42 -0500

    updated CORS

--- backendcode/main.py
+++ backendcode/main.py
@@ -11,7 +11,12 @@ import requests
 
 
 app = Flask(__name__)  # Create Flask app
-CORS(app, resources={r"/tossup": {"origins": "http://127.0.0.1:5500"}})
+CORS(app, resources={
+    r"/tossup": {"origins": "http://127.0.0.1:5500, https://anewprofile.github.io"},
+    r"/bonus": {"origins": "http://127.0.0.1:5500, https://anewprofile.github.io"},
+    r"/checkanswer": {"origins": "http://127.0.0.1:5500, https://anewprofile.github.io"},
+    r"/checkroomexists": {"origins": "http://127.0.0.1:5500, https://anewprofile.github.io"}
+})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 rooms = {
@@ -20,19 +25,19 @@ rooms = {
 
 
 @app.route('/tossup', methods=['GET'])  # Random tossup
-@cross_origin(origin='127.0.0.1',headers=['Content-Type','Authorization'])
+@cross_origin(origin='http://127.0.0.1:5500, https://anewprofile.github.io',headers=['Content-Type','Authorization'])
 def tossup():
     tossup = gc.get_tossup()  # Get tossup
     return json.dumps(tossup)
 
 @app.route('/bonus', methods=['GET'])  # Random bonus
-@cross_origin(origin='127.0.0.1',headers=['Content-Type','Authorization'])
+@cross_origin(origin='http://127.0.0.1:5500, https://anewprofile.github.io',headers=['Content-Type','Authorization'])
 def bonus():
     bonus = gc.get_bonuses()  # Get bonuses
     return json.dumps(bonus)
 
 @app.route('/checkanswer', methods=['GET'])  # Check answer
-@cross_origin(origin='127.0.0.1',headers=['Content-Type','Authorization'])
+@cross_origin(origin='http://127.0.0.1:5500, https://anewprofile.github.io',headers=['Content-Type','Authorization'])
 def checkanswer():
     question_id = request.args.get('questionid')  # get question id from query 
     question_obj = requests.get("https://qbreader.org/api/tossup-by-id", params={'id': question_id}).json()['tossup']  # get question object from API
@@ -44,7 +49,7 @@ def checkanswer():
     return json.dumps(ac.check_answer(user_input, correct_ans))
 
 @app.route('/checkroomexists', methods=['GET'])  # Check if room exists
-@cross_origin(origin='127.0.0.1', headers=['Content-Type', 'Authorization'])
+@cross_origin(origin='http://127.0.0.1:5500, https://anewprofile.github.io', headers=['Content-Type', 'Authorization'])
 def checkroomexists():
     room_id = request.args.get('roomid')  # get room id from query
     if rooms.__contains__(room_id):
